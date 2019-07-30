d30677547bdecf62d19e64b18b013fd14e4587db
minasora <1642940680@qq.com>
2019-07-30 15:59:20 +0800

    Initial commit

---
+++ src/start.java
@@ -0,0 +1,52 @@
+//绘制开始界面
+import javax.swing.*;
+public class start {
+    protected static void createAndshowGUI(){
+        try {
+
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");//Nimbus风格，jdk6 update10版本以后的才会出现
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());//当前系统风格
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");//Motif风格，是蓝黑
+            //UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());//跨平台的Java风格
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");//windows风格
+            //UIManager.setLookAndFeel("javax.swing.plaf.windows.WindowsLookAndFeel");//windows风格
+            //UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");//java风格
+            //UIManager.setLookAndFeel("com.apple.mrj.swing.MacLookAndFeel");//待考察，
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //风格设定
+        JFrame frame = new JFrame("fivesonchest");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,600);
+        //框架设定
+        JPanel panel = new JPanel();
+        frame.add(panel);
+        changePanel(panel,frame);
+        //面板设定
+
+        frame.setVisible(true);
+    }
+    private  static void changePanel(JPanel panel,JFrame frame)
+    {
+        panel.setLayout(null);//设定布局
+        ImageIcon imageicon = new ImageIcon("D:\\java learning\\java_game\\java_test1.0\\background.jpg");
+        JLabel lb = new JLabel(imageicon);
+        lb.setBounds(0,0,frame.getWidth(),frame.getHeight());
+        frame.getLayeredPane().add(lb,new Integer(Integer.MIN_VALUE));
+        JPanel j = (JPanel)frame.getContentPane();
+        j.setOpaque(false);
+        JButton userButton1 = new JButton("单人游戏");
+        JButton userButton2 = new JButton("多人游戏");
+        JButton userButton3 = new JButton("退出游戏");
+        userButton1.setBounds(250,100,300,100);
+        userButton2.setBounds(250,200,300,100);
+        userButton3.setBounds(250,300,300,100);
+        panel.add(userButton1);
+        panel.add(userButton2);
+        panel.add(userButton3);
+        panel.setOpaque(false);
+    }
+
+}
