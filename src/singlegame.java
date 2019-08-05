1aadb312eac392fbfaf03fd9eb25b29ab77a9537
minasora <1642940680@qq.com>
2019-08-06 00:26:50 +0800

    ver0.13 五子棋背景

--- src/singlegame.java
+++ src/singlegame.java
@@ -2,24 +2,34 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-class Gamepanel extends JPanel
-{
-    @Override
-    public void paintComponent(Graphics g)
-    {
-        super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        for(int i=0;i<=225;i+=15)
-            for(int j=10;j<=225;j+=15)
-            {
-                g2d.drawLine(i,j,225,225);
-            }
-        g2d.drawString("i love you ",500,200);
-    }
-}
 public class singlegame {
     protected static void modeSelect(JFrame frame){
-        frame.add(new Gamepanel());
+
+        JPanel jp = new JPanel();
+        jp.setLayout(null);
+        ImageIcon imageicon = new ImageIcon("background3.jpg");
+        JLabel lb = new JLabel(imageicon);
+        lb.setBounds(20,20,500,500);
+        jp.add(lb);
+        JButton userButton1 = new JButton("设定难度");
+        JButton userButton2 = new JButton("悔棋");
+        JButton userButton3 = new JButton("回到主菜单");
+        userButton1.setBounds(550,100,200,50);
+        userButton2.setBounds(550,150,200,50);
+        userButton3.setBounds(550,200,200,50);
+        jp.add(userButton1);
+        jp.add(userButton2);
+        jp.add(userButton3);
+        userButton3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.remove(jp);
+
+                frame.repaint();
+
+            }
+        });
+        frame.add(jp);
         frame.setVisible(true);
         frame.repaint();
     }
