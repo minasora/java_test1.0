8cc353f30026b2ecf8a815fc546f9189ff06353e
minasora <1642940680@qq.com>
2019-08-23 23:37:56 +0800

    time

---
+++ src/testsql.java
@@ -0,0 +1,12 @@
+import java.sql.*;
+
+public class testsql {
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/chest";
+    static final String USER = "root";
+    static final String PASS = "liboxiaoqq11qq";
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+    }
+}
